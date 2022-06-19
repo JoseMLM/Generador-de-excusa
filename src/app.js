76671c0abead0dbe6e4081b403e8d0863a61b1cf
JoseMLM <jmlopezmuro@gmail.com>
2022-06-19 09:41:01 +0000

    Ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let randomNumber = Math.floor(Math.random() * 10);
+  document.querySelector("#Pulsar").addEventListener("click", () => {
+    document.querySelector("#Excuse").innerHTML = generateExcuse();
+  });
+
+  let generateExcuse = () => {
+    let who = ["the dog", "my granma", "his turtle", "my bird"];
+    let what = ["eat", "pissed", "crushed", "broked"];
+    let when = [
+      "before the class",
+      "right in time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let whoIndex = Math.floor(Math.random() * who.length);
+    let whatIndex = Math.floor(Math.random() * what.length);
+    let whenIndex = Math.floor(Math.random() * when.length);
+
+    return who[whoIndex] + " " + what[whatIndex] + " " + when[whenIndex];
+  };
 };
