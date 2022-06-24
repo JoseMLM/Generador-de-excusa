2f8fbc3aec197a1cc9c8781b596ec178cf9099a3
JoseMLM <jmlopezmuro@gmail.com>
2022-06-24 18:45:52 +0000

    Ejercicio corregido

--- src/app.js
+++ src/app.js
@@ -6,26 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = () => {
-  let randomNumber = Math.floor(Math.random() * 10);
-  document.querySelector("#Pulsar").addEventListener("click", () => {
-    document.querySelector("#Excuse").innerHTML = generateExcuse();
-  });
+  document.querySelector("#Excuse").innerHTML = generateExcuse();
+};
 
-  let generateExcuse = () => {
-    let who = ["the dog", "my granma", "his turtle", "my bird"];
-    let what = ["eat", "pissed", "crushed", "broked"];
-    let when = [
-      "before the class",
-      "right in time",
-      "when I finished",
-      "during my lunch",
-      "while I was praying"
-    ];
+document.querySelector("#Pulsar").addEventListener("click", () => {
+  document.querySelector("#Excuse").innerHTML = generateExcuse();
+});
 
-    let whoIndex = Math.floor(Math.random() * who.length);
-    let whatIndex = Math.floor(Math.random() * what.length);
-    let whenIndex = Math.floor(Math.random() * when.length);
+let generateExcuse = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-    return who[whoIndex] + " " + what[whatIndex] + " " + when[whenIndex];
-  };
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return who[whoIndex] + " " + what[whatIndex] + " " + when[whenIndex];
 };
